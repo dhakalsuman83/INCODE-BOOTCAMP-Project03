d2776decf2ca2f1d3331ccc273693a701556cbc1
dhakalsuman83 <dhakalsuman83@gmail.com>
2021-08-27 16:25:57 +1000

    half-way through the project 3b

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const bcrypt = require('bcryptjs')
 const app = express()
 const data = require('./data')
+const expressLayouts = require("express-ejs-layouts")
+
 
 
 const PORT = process.env.PORT || 3000
@@ -12,27 +14,47 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+//setting the template engine
+app.set("view engine", "ejs")
+
+app.use(expressLayouts)
+app.set("layout","./layouts/layout")
+
+
+
 //‘/', which returns the message "Welcome to our schedule website".
 app.get("/", (req, res)=> {
-    res.send("Welcome to our schedule website")
+    res.render("./pages/home")
 })
 
 
 //‘/users’, which returns the list of users
 app.get("/users", (req, res) => {
-    res.send(data.users)
+    res.render("./pages/users", {
+        users : data.users
+    })
 })
 
 
 //‘/schedules’, which returns the list of schedules
 app.get("/schedules", (req, res) => {
-    res.send(data.schedules)
+    res.render("./pages/schedules" , {
+        schedules: data.schedules})
 })
 
 
 //the URL '/users/2' will return the information of user n°2
 app.get("/users/:id", (req, res) => {
-    res.send (data.users[parseInt (req.params.id)])
+    input = req.params.id
+    if (input === "new") {
+        res.render("./pages/user-form")
+    } else {
+    res.render("./pages/specific-user", {
+        user : data.users[parseInt(req.params.id)]
+    })
+    }
+        
+    
 })
 
 //the URL '/users/2/schedules' will return a list of all schedules for user n°2
@@ -40,30 +62,36 @@ app.get("/users/:id/schedules", (req, res) => {
     const id = parseInt(req.params.id)
     tempSchedules = data.schedules.filter(schedule => schedule.user_id === id)
     //console.log(tempSchedules)
-    res.send(tempSchedules)
+    res.render("./pages/specific-schedules", {
+        tempSchedules
+    })
 })
+
 
 
 //‘/schedules’ to add a new schedule. It will return the newly created schedule.
-app.post("/schedules", (req, res) => {
-    const newSchedules = req.body
-    data.schedules.push(newSchedules)
-    res.send(data.schedules)
+// app.post("/schedules", (req, res) => {
+//     const newSchedules = req.body
+//     data.schedules.push(newSchedules)
+//     res.send(data.schedules)
     
-})
+// })
 
 
-//‘/users’ (this time in POST!) to add a new user. It will return the newly created user. The user's password must be encrypted in bcrypt.
-app.post("/users", (req, res) => {
-    const { firstname, lastname, email, password } = req.body
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
-    console.log(hash)
-    const newUsers ={ firstname, lastname, email, password:hash }
-    data.users.push(newUsers)
-    res.send(data.users)
+// //‘/users’ (this time in POST!) to add a new user. It will return the newly created user. The user's password must be encrypted in bcrypt.
+// app.post("/users", (req, res) => {
+//     const { firstname, lastname, email, password } = req.body
+//     const salt = bcrypt.genSaltSync(10)
+//     const hash = bcrypt.hashSync(password, salt)
+//     console.log(hash)
+//     const newUsers ={ firstname, lastname, email, password:hash }
+//     data.users.push(newUsers)
+//     res.send(data.users)
     
-})
+// })
+
+
+
 
 
 
